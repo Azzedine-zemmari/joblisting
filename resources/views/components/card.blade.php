596d6badb1a7be296053541ad4c979f545636815
Azzedine zemmari <98091150+Azzedine-zemmari@users.noreply.github.com>
2024-03-02 11:44:28 +0100

    Tags Component

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,4 +1,4 @@
 <div {{$attributes->merge(['class'=>'bg-gray-50 border border-gray-200 rounded p-6'])}}>
     {{$slot}}
 </div>
-{{--this $attributes->merge let me pass new class if i need like this <x-card class="p-10">}}
+{{--this $attributes->merge let me pass new class if i need like this <x-card class="p-10">--}}
