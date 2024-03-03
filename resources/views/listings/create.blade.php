60f262170fae9ba6c1223d64a0034218b9e0de71
Azzedine zemmari <98091150+Azzedine-zemmari@users.noreply.github.com>
2024-03-03 12:03:12 +0100

    keep old data in form

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -12,7 +12,7 @@
                         <div class="mb-6">
                             <label for="company" class="inline-block text-lg mb-2">Company Name</label>
                             <div class="relative">
-                                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="company" placeholder="Enter company name">
+                                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="company" value="{{old('company')}}" placeholder="Enter company name">
                                 <svg class="absolute right-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-500" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                     <path d="M12 2c3.866 0 7 3.134 7 7s-3.134 7-7 7-7-3.134-7-7 3.134-7 7-7zM5.58 17c1.507-1.788 3.685-3 6.42-3 2.735 0 4.913 1.212 6.42 3H5.58zm2.813-5.427c1.31 1.053 3.053 1.612 4.607 1.612s3.297-.559 4.607-1.612c1.556-1.25 2.387-3.002 2.387-4.873C19.984 5.69 17.294 4 14 4s-5.984 1.69-5.984 2.688c0 1.871.831 3.623 2.387 4.885zM12 22s-8-4.5-8-8c0-2.935 2.02-5.475 4.958-6.446A8.004 8.004 0 0112 2c4.418 0 8 3.582 8 8s-8 10-8 10z" />
                                 </svg>
@@ -30,6 +30,7 @@
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="title"
                                 placeholder="Example: Senior Laravel Developer"
+                                value="{{old('title')}}"
                             />
                             @error('title')
                                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -46,6 +47,7 @@
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="location"
+                                value="{{old('location')}}"
                                 placeholder="Example: Remote, Boston MA, etc"
                             />
                             @error('location')
@@ -61,6 +63,7 @@
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="email"
+                                value="{{old('email')}}"
                             />
                             @error('email')
                                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -78,6 +81,7 @@
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="website"
+                                value="{{old('website')}}"
                             />
                             @error('website')
                                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -92,6 +96,7 @@
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="tags"
+                                value="{{old('tags')}}"
                                 placeholder="Example: Laravel, Backend, Postgres, etc"
                             />
                             @error('tags')
@@ -107,6 +112,7 @@
                                 type="file"
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="logo"
+                                value="{{old('logo')}}"
                             />
                             @error('logo')
                             <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -123,6 +129,7 @@
                             <textarea
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="description"
+                                value="{{old('description')}}"
                                 rows="10"
                                 placeholder="Include tasks, requirements, salary, etc"
                             ></textarea>
