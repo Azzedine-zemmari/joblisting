3b01f62a0ce073ca09e0de472431804484d5216f
Azzedine zemmari <98091150+Azzedine-zemmari@users.noreply.github.com>
2024-03-03 11:54:36 +0100

    Alpine js for message removal

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -12,6 +12,7 @@
             crossorigin="anonymous"
             referrerpolicy="no-referrer"
         />
+        <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
         <script src="https://cdn.tailwindcss.com"></script>
         <script>
             tailwind.config = {
